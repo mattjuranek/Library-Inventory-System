ddd07c28b0bd604ebc0f007e10b74d1f8b1cf997
mattjuranek <juranekm@ufl.edu>
2024-07-05 11:16:57 -0400

    Implemented JWT functionality for user authentication

---
+++ backend/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('./db');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('./User')
+
+app.use(express.json())
+
+app.get('/profile', authenticateToken, async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.user.name });
+        if (!user) return res.sendStatus(404);
+        res.json({ username: user.username });
+    } catch (error) {
+        res.status(500).send('Error fetching user profile');
+    }
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+app.listen(3000)
